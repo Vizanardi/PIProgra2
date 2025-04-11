7ff5808556738a04ae8ff1de4a20d130b0f12dca
Vizanardi <mzanardiocampo@udesa.edu.ar>
2025-04-11 10:24:19 -0300

    puntos 5,6,7,8 hechos

---
+++ routes/profile.js
@@ -0,0 +1,7 @@
+const express = require("express")
+const router = express.Router()
+const ProfileController = require()
+
+router.get()
+
+module.exports= router
---
+++ routes/register.js
@@ -0,0 +1,7 @@
+const express = require("express")
+const router = express.Router()
+const RegisterController = require()
+
+router.get()
+
+module.exports= router
